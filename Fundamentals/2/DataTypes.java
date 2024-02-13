8eb684e22cb6da13ff2a9f1a663cc39ea3f268ed
Ruturaj Panditrao <94856890+Ruturaj-Panditrao@users.noreply.github.com>
2024-02-13 14:48:00 +0530

    Update DataTypes.java

--- Fundamentals/2/DataTypes.java
+++ Fundamentals/2/DataTypes.java
@@ -105,20 +105,96 @@
 // But if we write as int x = 2^32L
 // Then we get the compile time error as found long, required int.
 
-// long
-// float
-// double
-// char
-// boolean
+// 4> long
 
+// 8 bytes : 64 bits
+// Range : -2^63 to 2^63-1
+// If 'f' is the length of a file,
+// Then f.length() returns the number of characters in the file.
+// The file can contain thousands of lines and each line can have thousands of characters
+// Hence the right datatype to store f.length() is long and not int
+// The return type of this length method is long and not int
+// long l = f.length()
+
+// 5> float
+// 6> double
+// While deciding to take byte, short, int, long
+// We decide on the basis of the size 
+// However to decide among float and double
+// We decide on the basis of precision we want
+// For 5-6 decimal place precision, use float
+// For 14-15 decimal place precision, use double
+// Float follows Single Precision
+// Double follows Double Precision
+// Float : 4 bytes : 32 bits
+// Range -3.4e38 to 3.4e38 (e38 is 10^38)
+// Double : 8 bytes : 64 bits
+// Range -1.7e308 to 1.7e308 (e308 is e^308)
+
+// 7> boolean
+// We cannot say the size of boolean,ie size of boolean is inapplicable in Java
+// The size of Boolean is actually Virtual Machine Dependent
+// Hence, range of Boolean is also not applicable in Java
+// boolean b = 0
+// Not allowed : Incompatible Types 
+// Found integer, Required Boolean
+// boolean b =  True
+// True is not boolean not string as is not in double quotes
+// Compile Time Error : cannot find symbol
+// Symbol : True
+// Location : class Test
+
+// boolean b = "True"
+// Compile Time Error : Incompatible Type
+// Found : String Required : Boolean
+// char
 // In boolean and char, minus sign does not have a meaning.
 // Hence, except Boolean and Char, remaining datatypes are considered as Signed datatypes
 // Because we can have both positive and negative types
 
+// 0 means false in old programming languages not in Java
+// 1 means true in old programming languages not in Java
+
+int x = 0;
+if(x) System.out.println("Hello");
+else System.out.println("Bye");
+
+// Above works in C++, but it will not work in Java.
+// It will throw a compile time error : Incompatible types
+
+while(1)
+{
+  System.out.println("Hello World");
+}
+
+// Above will print infinitely in C++
+// But it will throw a compile time error in Java
+
+// 8> char
+
+// In C/C++, size of char is 1 Byte
+// Because they are ASCII based
+// 256 different symbols or values,
+// Hence 8 bits are sufficient
+// In Java, size of char is 2 Bytes
+// Because they are Unicode based
+// First 8 bits are used for 256 Ascii codes
+// The next 8 bits are used for any language symbols that are internationally accepted
 
 
+// Datatype      Size      Wrapper Class      Default Value
+// byte          1 byte       Byte                  0
+// short         2 byte       Short                 0
+// int           4 byte      Integer                0
+// long          8 byte       Long                  0
+// float         4 byte       Float                0.0
+// double        8 byte       Double               0.0
+// boolean         NA         Boolean              false
+// char          2 byte      Character              0
 
-
+// 0 in Character represents the Space character
+// null is the default value for object reference and cannot use for primitives
+// Example char ch = null is invalid or incompatible
 
 
 
