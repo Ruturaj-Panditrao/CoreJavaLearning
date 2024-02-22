c76e1ed59be06d072eda1f15c76c736a5fffb7a6
Ruturaj Panditrao <94856890+Ruturaj-Panditrao@users.noreply.github.com>
2024-02-22 12:22:00 +0530

    Create 1.java

---
+++ Flow_Control/1.java
@@ -0,0 +1,19 @@
+// Flow control is the order in which statements will be executed at runtime
+
+// Selection Statements
+// 1) if-else
+// 2) switch
+
+// Iterative Statements
+// 1) while
+// 2) do-while
+// 3) for
+// 4) for-each
+
+// Transfer Statements
+// 1) break
+// 2) continue
+// 3) return
+// 4) try-catch-finally
+// 5) assert 
+
